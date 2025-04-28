7b01102b365116846ae2b663af8e8de42c366011
Samuelijah <samuelijah456@gmail.com>
2025-04-28 17:07:48 +0100

    first commit

---
+++ src/Pages/Home/Technologies.jsx
@@ -0,0 +1,30 @@
+import data from "../../data/index.json";
+
+export default function Technologies() {
+  return (
+       <section className="technologies--section" id="mySkills">
+         <div className="portfolio--container">
+           <p className="sub--title">Technologies</p>
+           <h2 className="sections--heading">Technology Stacks</h2>
+         </div>
+         <div className="technologies--section--container">
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+           <img src="./img/tag-1.png"/>
+         </div>
+       </section>
+     );
+}
